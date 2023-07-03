803b091586d2babb7d1f0d361464a66b52967a6e
salharazick <salharazick2008@gmail.com>
2023-07-03 23:16:35 +0530

    added filter and pagination

--- app/Http/Controllers/productsController.php
+++ app/Http/Controllers/productsController.php
@@ -6,20 +6,42 @@ use App\Http\Requests\productStoreRequest;
 use App\Http\Requests\productUpdateRequest;
 use App\Http\Resources\productResource;
 use App\Models\product;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class productsController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource with filter and pagination.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::paginate(2);
-        return response()->json(productResource::collection($products));
+
+        $category = $request->query('category_id');
+        $perPage = $request->query('paginate');
+
+        $query = Product::query();
+
+        if ($category) {
+            $query->where('category_id', $category);
+        }
+
+        $products = $query->paginate($perPage);
+
+       
+        $nextPageUrl = $products->appends($request->query())->nextPageUrl();
+
+        return response()->json([
+        'data' => productResource::collection($products),
+        'next_page_url' => $nextPageUrl,
+    ]);
+
     }
+
 
     /**
      * Store a newly created resource in storage.
@@ -29,18 +51,33 @@ class productsController extends Controller
      */
 
     public function store(productStoreRequest $request)
-    {
-    $validator = Validator::make($request->all(), $request->rules());
+     {
+        try {
+            $validator = Validator::make($request->all(), $request->rules());
+     
+            if ($validator->fails()) {
+                return response()->json([
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+             
+            $product = product::create($request->validated());
+     
+            return response()->json(new productResource($product), 201);
+            } 
 
-        if ($validator->fails()) {
+        catch (ValidationException $exception) {
             return response()->json([
-                'errors' => $validator->errors()
+                'errors' => $exception->errors()
             ], 422);
-        }
-    $product = product::create($request->validated());
+            }
 
-    return response()->json(new productResource($product), 201);
-   }
+        catch (\Exception $exception) {
+            Log::error($exception);
+            return response()->json(['error' => 'An error occurred while storing the product.'], 500);
+        }
+
+    }
     
 
     /**
@@ -63,10 +100,18 @@ class productsController extends Controller
      */
     public function update(productUpdateRequest $request, product $product)
     {
-        
-        $product->update($request->validated());
 
+    try {
+        $product->update($request->validated());
         return response()->json(new productResource($product));
+        }
+
+    catch (\Exception $exception)
+        {
+        Log::error($exception);
+        return response()->json(['error' => 'An error occurred during the update.'], 500);
+        }
+
     }
 
     /**
